4611f9a4b2fb5ae3f15e0a759ce233de8518f1b3
Bhargavi <bhargavishetty113@gmail.com>
2021-10-03 20:36:13 +0530

    updated

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
-import {getPosts , createPost , deletePost} from '../controllers/post.js'
+import {getPosts , createPost ,updatePost,likePost , deletePost} from '../controllers/post.js'
 
 router.post('/',createPost);
 router.get('/', getPosts);
+router.patch('/:id' , updatePost);
 router.delete('/:id' , deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
